7bb4a68ca6ffaf66a7c5226fcce8353d051ea183
zyn <zaynassaf2001@gmail.com>
2026-03-02 20:54:37 +0200

    new code

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
+    plugins: { js, security: pluginSecurity },
     extends: ["js/recommended"],
+    rules: {
+      ...pluginSecurity.configs.recommended.rules,  // ← add this
+    },
     languageOptions: {
-      globals: { ...globals.node, ...globals.es2021 } // ⚡ هنا عرفنا Node و ES2021
+      globals: { ...globals.node, ...globals.es2021 }
     }
   },
   tseslint.configs.recommended,
