0541d413f1d262c8b251c512681c5087c596c8ee
zyn <zaynassaf2001@gmail.com>
2026-03-02 19:23:12 +0200

    test: add unused function to trigger lint error

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.es2021 } // ⚡ هنا عرفنا Node و ES2021
+    }
+  },
+  tseslint.configs.recommended,
+]);
